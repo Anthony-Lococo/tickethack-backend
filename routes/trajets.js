046365307ff449bb6b6c500d53215a6c997ef249
Maxime <max.boistelle@hotmail.fr>
2024-04-16 12:23:17 +0200

    end route trajet + end router.get

--- routes/trajets.js
+++ routes/trajets.js
@@ -1,18 +1,30 @@
 var express = require("express");
 var router = express.Router();
 const Trajets = require("../models/trajets");
-const mongoose = require("moongoose");
+require("../models/connection");
 const { checkFields } = require("../modules/checkFields");
 
-router.post("/trajets", (res, req) => {
-  if (checkFields(req.body, ["departure", "arrival"]) === false) {
+router.post("/search", (req, res) => {
+  let findTravel = [];
+  if (checkFields(req.body, ["departure", "arrival", "date"]) === false) {
     res.json({ result: false, message: "Empty fields" });
   } else {
     Trajets.find({
-      departure: new RegExp(`${req.body.departure}.*`, `i`),
+      departure: new RegExp(`${req.body.departure}`, `i`),
     }).then((data) => {
-      if (!data) {
+      if (data.length === 0) {
         res.json({ result: false, message: "No trip found" });
+      } else {
+        for (const value of data) {
+          const reqDate = new Date(req.body.date);
+          if (reqDate <= value.date) {
+            if (req.body.arrival === value.arrival) {
+              findTravel.push(value);
+              console.log(value);
+            }
+          }
+        }
+        res.json({ result: true, message: findTravel });
       }
     });
   }
