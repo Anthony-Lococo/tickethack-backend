f27abb2dcddf16e15d2163a6f933138bd4bf95f8
Maxime <max.boistelle@hotmail.fr>
2024-04-16 11:05:53 +0200

    router.get first part

--- routes/trajets.js
+++ routes/trajets.js
@@ -1,4 +1,21 @@
 var express = require("express");
 var router = express.Router();
+const Trajets = require("../models/trajets");
+const mongoose = require("moongoose");
+const { checkFields } = require("../modules/checkFields");
+
+router.post("/trajets", (res, req) => {
+  if (checkFields(req.body, ["departure", "arrival"]) === false) {
+    res.json({ result: false, message: "Empty fields" });
+  } else {
+    Trajets.find({
+      departure: new RegExp(`${req.body.departure}.*`, `i`),
+    }).then((data) => {
+      if (!data) {
+        res.json({ result: false, message: "No trip found" });
+      }
+    });
+  }
+});
 
 module.exports = router;
