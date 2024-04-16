029657cca02e3cc0fefd256aab5a228026dd87c6
Maxime <max.boistelle@hotmail.fr>
2024-04-16 11:06:11 +0200

    create route in app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var trajetsRouter = require("./routes/trajets");
 
 var app = express();
 const cors = require("cors");
@@ -17,5 +18,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/trajets", trajetsRouter);
 
 module.exports = app;
